fe86ca6c12e0b01837dc5febeebf91eb0915d66e
Firedrago95 <yonghwa95@naver.com>
2026-04-15 18:27:28 +0900

    chore: 초기 프로젝트 설정 및 기본 인프라 구성 추가
    - Spring Boot 기반 build.gradle 초기 설정 추가
    - MySQL, Redis, Kafka docker-compose 구성 추가
    - 프로젝트 실행 및 구조 설명을 위한 README 생성
    - 기본 개발 환경 구성을 위한 설정 파일 초기화

---
+++ src/main/java/com/realteeth/assignment/ImageProcessingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.realteeth.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageProcessingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageProcessingSystemApplication.class, args);
+	}
+
+}
