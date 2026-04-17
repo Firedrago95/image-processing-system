3597cd9a45011b4c5bedf4d0ba53086e170c09d6
Firedrago95 <yonghwa95@naver.com>
2026-04-17 16:21:11 +0900

    feat: KafkaConfig 클래스 추가로 Virtual Thread 기반 설정 구현
    - KafkaListenerFactory에 Virtual Thread 사용 설정 추가
    - SimpleAsyncTaskExecutor를 활용한 비동기 스레드 구성 적용
    - ConsumerFactory를 활용한 KafkaListener 컨테이너 생성 로직 추가
    - 어노테이션 기반 @Configuration으로 빈 등록 처리
    - Virtual Thread 적용을 통해 리스너 성능 최적화 시도

---
+++ src/main/java/com/realteeth/assignment/global/config/KafkaConfig.java
@@ -0,0 +1,26 @@
+package com.realteeth.assignment.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory (
+        ConsumerFactory<String, String> consumerFactory
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+
+        SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor("kafka-vt-");
+        executor.setVirtualThreads(true);
+
+        factory.getContainerProperties().setListenerTaskExecutor(executor);
+
+        return factory;
+    }
+}
