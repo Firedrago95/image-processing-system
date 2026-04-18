03f219c7f67085af3da02664fafb33b8da621695
Firedrago95 <yonghwa95@naver.com>
2026-04-18 14:17:08 +0900

    chore: Redis 관련 설정 및 의존성 제거
    - application.yaml에서 Redis 설정 제거
    - docker-compose.yml에서 Redis 서비스 정의 삭제
    - build.gradle에서 Redis 관련 의존성 제거
    - IntegrationTestSupport에서 Redis 컨테이너 설정 제거
    - 불필요한 Redis 리소스 제거로 전체 환경 단순화

--- src/test/java/com/realteeth/assignment/support/IntegrationTestSupport.java
+++ src/test/java/com/realteeth/assignment/support/IntegrationTestSupport.java
@@ -3,7 +3,6 @@ package com.realteeth.assignment.support;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.lifecycle.Startables;
@@ -16,9 +15,6 @@ public class IntegrationTestSupport {
     @ServiceConnection
     static final MySQLContainer<?> MYSQL;
 
-    @ServiceConnection(name = "redis")
-    static final GenericContainer<?> REDIS;
-
     @ServiceConnection
     static final KafkaContainer KAFKA;
 
@@ -28,11 +24,8 @@ public class IntegrationTestSupport {
             .withUsername("test")
             .withPassword("test");
 
-        REDIS = new GenericContainer<>(DockerImageName.parse("redis:7.2-alpine"))
-            .withExposedPorts(6379);
-
         KAFKA = new KafkaContainer(DockerImageName.parse("apache/kafka:3.7.0"));
 
-        Startables.deepStart(MYSQL, REDIS, KAFKA).join();
+        Startables.deepStart(MYSQL, KAFKA).join();
     }
 }
